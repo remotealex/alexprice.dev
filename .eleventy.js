f600e99937909825363d8e8a9eaa9b46f4a0971b
Alex Price <hi@alexpriceonline.com>
2020-04-01 07:13:21 +0100

    Updated eleventy config to include favicons in build

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
 module.exports = function(eleventyConfig) {
-  
+
   /* Pass through - stop eleventy touching */
-  eleventyConfig.addPassthroughCopy('src/images')
+  // eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/android-chrome-192x192.png');
+  eleventyConfig.addPassthroughCopy('src/android-chrome-512x512.png');
+  eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('src/favicon-16x16.png');
+  eleventyConfig.addPassthroughCopy('src/favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
 
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
